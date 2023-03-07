919655ae9d57aa00b2e27583125592bd3317b2d8
Ani Obinna Benzeal <101447833+mrbenzeal@users.noreply.github.com>
2023-03-08 00:59:33 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""Amenity class that inherits from BaseModel"""
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Our beautiful Amenity class"""
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
     name = ""
