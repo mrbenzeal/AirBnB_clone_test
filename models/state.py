55141e7659917e3f86ace0c78ca9bee1762633fa
Ani Obinna Benzeal <101447833+mrbenzeal@users.noreply.github.com>
2023-03-08 01:05:40 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""State class that inherits from BaseModel"""
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Our beautiful State class"""
+    """Represent a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
     name = ""
