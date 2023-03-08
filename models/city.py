a26e340a3324bd37345db4020c86b67c9c0ac5fa
Ani Obinna Benzeal <101447833+mrbenzeal@users.noreply.github.com>
2023-03-08 01:01:40 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""City class that inherits from BaseModel"""
+"""Defines the City class."""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Our beautiful City class"""
+    """Represent a city.
+
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
     state_id = ""
     name = ""
